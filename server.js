c3ac41087d35ae0bec98bee8f80e323d9e4e47d6
Norman Poll Quijandria Pittman <norman.quijandria@nirex.pe>
2018-02-08 00:39:38 -0500

    Databas added

--- server.js
+++ server.js
@@ -107,5 +107,5 @@ app.use(express.static(__dirname + '/views'));
 
 var port = process.env.PORT || 3000
 app.listen(port, function() {
-    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+    console.log("To view your app, open this link in your browser!: http://localhost:" + port);
 });
